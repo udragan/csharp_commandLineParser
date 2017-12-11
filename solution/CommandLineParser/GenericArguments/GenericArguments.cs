a3a2ffd3c2d3c7e9d22d58ef611cdb7dc5182825
udragan <ujfa2001@yahoo.com>
2017-12-11 18:12:58 +0100

    added GenericArguments base class.

---
+++ solution/CommandLineParser/GenericArguments/GenericArguments.cs
@@ -0,0 +1,17 @@
+﻿namespace com.udragan.csharp.CommandLineParser.Options
+{
+	/// <summary>
+	/// Base class for generic arguments.
+	/// </summary>
+	public class GenericArguments
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="GenericArguments"/> class.
+		/// </summary>
+		/// <param name="args">The arguments.</param>
+		public GenericArguments(string[] args)
+		{
+
+		}
+	}
+}
