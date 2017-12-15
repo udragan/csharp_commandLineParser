619283fae7f47d035b07949b1d4db2e7a15de312
udragan <ujfa2001@yahoo.com>
2017-12-15 19:30:57 +0100

    added BaseAttribute class.

---
+++ solution/CommandLineParser/Attributes/BaseAttribute.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace com.udragan.csharp.CommandLineParser.Attributes
+{
+	/// <summary>
+	/// Base attribute.
+	/// </summary>
+	/// <seealso cref="System.Attribute" />
+	public abstract class BaseAttribute : Attribute
+	{
+		#region Members
+
+		private string _optionName;
+		private string _help;
+		private bool _required;
+
+		#endregion
+
+		#region Constructors
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="BaseAttribute"/> class.
+		/// </summary>
+		/// <param optionName="optionName">The option name.</param>
+		/// <param optionName="help">The help text.</param>
+		public BaseAttribute(string optionName, string help)
+		{
+			_optionName = optionName;
+			_help = help;
+			_required = false;
+		}
+
+		#endregion
+
+		#region Properties
+
+		/// <summary>
+		/// Gets the switch option name.
+		/// </summary>
+		public string OptionName
+		{
+			get { return _optionName; }
+		}
+
+		/// <summary>
+		/// Gets the help text.
+		/// </summary>
+		public string Help
+		{
+			get { return _help; }
+		}
+
+		/// <summary>
+		/// Gets or sets a value indicating whether this argument is required.
+		/// </summary>
+		public bool Required
+		{
+			get { return _required; }
+			set { _required = value; }
+		}
+
+		#endregion
+
+		#region Overrides
+
+		public override int GetHashCode()
+		{
+			return _optionName.GetHashCode();
+		}
+
+		#endregion
+	}
+}
