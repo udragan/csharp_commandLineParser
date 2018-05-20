29a30c32a694eac94b1253e57c329c8e0171ed42
udragan <ujfa2001@yahoo.com>
2018-05-20 11:22:55 -0700

    tfs test commit.

--- solution/TestApp/Loggers/FileLogger.cs
+++ solution/TestApp/Loggers/FileLogger.cs
@@ -14,6 +14,7 @@ namespace com.udragan.csharp.TestApp.Loggers
 
 		private readonly string _fileName;
 		private readonly string _filePath;
+
 		#endregion
 
 		#region Constructors
@@ -25,7 +26,6 @@ namespace com.udragan.csharp.TestApp.Loggers
 		public FileLogger(string fileName)
 			: this(fileName, Directory.GetCurrentDirectory())
 		{ }
-
 
 		/// <summary>
 		/// Initializes a new instance of the <see cref="FileLogger"/> class.
